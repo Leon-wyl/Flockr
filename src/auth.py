2396e77c100ffffcb82a5d17b2f8bc715765d868
Yilang Wu <z5314144@ad.unsw.edu.au>
2020-09-28 20:50:21 +1000

    implemented auth_register and auth_login functions

--- src/auth.py
+++ src/auth.py
@@ -1,16 +1,78 @@
+import re
+from src.database import data
+
+regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+
 def auth_login(email, password):
+    '''Given a registered users' email and 
+    password and generates a valid token 
+    for the user to remain authenticated'''
+    correct_user = None
+    for user in data['users']:
+        if user['email'] == email:
+            correct_user = user
+
+    if correct_user == None:
+        raise Exception(f"Error, email address {email} has not been registered yet")
+
+    if correct_user['password'] != password:
+        raise Exception(f"Error, wrong password")
+    
     return {
-        'u_id': 1,
-        'token': '12345',
+        'u_id': correct_user['u_id'],
+        #'token': '12345',
     }
 
 def auth_logout(token):
+    '''Given an active token, invalidates the taken to 
+    log the user out. If a valid token is given, and the 
+    user is successfully logged out, it returns true, 
+    otherwise false.'''
     return {
         'is_success': True,
     }
 
 def auth_register(email, password, name_first, name_last):
+    '''Given a user's first and last name, email address, and password, create a new account
+    for them and return a new token for authentication in their session. A handle is generated
+    that is the concatentation of a lowercase-only first name and last name. If the
+    concatenation is longer than 20 characters, it is cutoff at 20 characters. If the handle 
+    is already taken, you may modify the handle in any way you see fit to make it unique.'''
+    if not (re.search(regex,email)):  
+        raise Exception(f"Email entered is not a valid email")
+
+    for user in data['users']:
+        if user['email'] == email:
+            raise Exception(f"Email address {email} is already being used by another user")
+    
+    if len(password) in range(0, 6):
+        raise Exception("Password entered is less than 6 characters long")
+
+    if len(name_first) not in range(1, 51):
+        raise Exception("name_first is not between 1 and 50 characters inclusively in length")
+
+    if len(name_last) not in range(1, 51):
+        raise Exception("name_last is not between 1 and 50 characters inclusively in length")
+
+    u_id = len(data['users'])
+
+    handle = (name_first + name_last).lower()
+    for user in data['users']:
+        if user['handle'] == handle[:20]:
+            handle = handle[:6] + u_id
+            handle = handle[:20]
+            break
+
+    data['users'].append({
+        'u_id': u_id,
+        'email': email,
+        'password': password
+        'name_first': name_first,
+        'name_last': name_last,
+        'handle': handle,
+    })
+
     return {
-        'u_id': 1,
-        'token': '12345',
+        'u_id': u_id,
+        #'token': '12345',
     }
--- src/test_auth.py
+++ src/test_auth.py
@@ -5,18 +5,22 @@ from src.database import data
 def test_register():
     # Valid information has been summitted to register from the first user
     info = auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "Wu")
-    assert info =={'u_id': 0, }
+    assert info =={'u_id': 0}
 
     # Vadid information has been summitted to register from the second user
     info = auth_register("billgates@outlook.com", "VukkFsrwa", "Bill", "Gates")
-    assert info =={'u_id': 1, }
+    assert info =={'u_id': 1}
 
     # Vadid information has been summitted to register from the third user
     info = auth_register("Monique.Johnson@icloud.com", "RFVtgb45678", "Monique", "Johnson")
-    assert info =={'u_id': 2, }
+    assert info =={'u_id': 2}
 
     # Test the number of users
-    assert len(data['users']) == 2
+    assert len(data['users']) == 3
+
+    # Invalid email
+    with pytest.raise(Exception):
+        auth_register("ufhsdfkshfdhfsfhiw", "uf89rgu", "Andrew", "Williams")    
 
     # Email has already used to register by another users
     auth_register("uniisfun@ad.unsw.edu.au", "ILoveUniversity", "Hayden", "Smith")
