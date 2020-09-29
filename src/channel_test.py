821d700f6868c3de8585bab109027a072ad1a9c2
Guanbin <z5285766@student.unsw.edu.au>
2020-09-29 17:27:54 +1000

    Finished 1st stage of testing

--- src/channel_test.py
+++ src/channel_test.py
@@ -2,6 +2,7 @@ import channel
 import pytest
 import error
 import channels
+from message import message_send
 
 def test_echo():
     assert echo.echo("1") == "1", "1 == 1"
@@ -12,22 +13,57 @@ def test_echo_except():
     with pytest.raises(InputError) as e:
         assert echo.echo("echo")
 
-def test_invalid_channel():
+def test_invalid_id_channel_details():
     result = channels.channels_create('token', 
              'validchannelname', 'is_public')
     with pytest.raises(InputError) as e:
-        channel.channel_details('token', 'channel_id')
+        assert channel.channel_details('token', '999')
         
-def test_unauthorised_channel():
+def test_unauthorised_channel_details():
     result = channels.channels_create('anothertoken', 
              'validchannelname', 'is_private')
     with pytest.raises(AccessError) as a:
-        channel.channel_details('token', 'channel_id')  
+        assert channel.channel_details('token', 'channel_id')  
       
 def test_channel_details():
     result = channels.channels_create('token', 
              'validchannelname', 'is_public')
-    assert channel.channel_details("token", "channel_id") == "validchannelname", "{ name, owner_members, all_members }"
-    assert echo.echo("abc") == "abc", "abc == abc"
-    assert echo.echo("trump") == "trump", "trump == trump"        
+    assert channel.channel_details("token", "channel_id") == "validchannelname", "owner_members", "all_members"
+
+def test_invalid_id_channel_messages():
+    result = channels.channels_create('token', 
+             'validchannelname', 'is_public')
+    with pytest.raises(InputError) as e:
+        assert channel.channel_messages('token', 'wrong_channel_id',
+                                        'start')
+        
+def test_invalid_start_channel_messages():
+    result = channels.channels_create('token', 
+             'validchannelname', 'is_public')
+    with pytest.raises(InputError) as e:
+        assert channel.channel_messages('token', 'channel_id', '999')
+        
+def test_unauthorised_channel_messages():
+    result = channels.channels_create('anothertoken', 
+             'validchannelname', 'is_private')
+    with pytest.raises(AccessError) as a:
+        assert channel.channel_messages('token', 'channel_id', 'start') 
+        
+def test_channel_messages():
+    result = channels.channels_create('token', 
+             'validchannelname', 'is_public')
+    message.message_send('token', 'channel_id', 'message')
+    assert channel.channel_messages("token", "channel_id", 'start') ==  
+                                    "message", 'start', 'end'
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+           
 
