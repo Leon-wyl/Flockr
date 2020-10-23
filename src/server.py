48462f937410e66c641ff901a25290916867970f
Yilang Wu <z5314144@ad.unsw.edu.au>
2020-10-23 13:22:06 +1100

    the server of auth.py is implemented in server.py

--- src/server.py
+++ src/server.py
@@ -32,5 +32,23 @@ def echo():
         'data': data
     })
 
+@APP.route('/auth/login', methods=['POST'])
+def server_login():
+    data = request.get_json()
+    if 'email' in data and 'password' in data:
+        return dumps(auth_login(data['email'], data['password']))
+    raise InputError(description='invalid input')
+
+@APP.route('/auth/logout', methods=['POST'])
+def server_logout():
+    data = request.get_json()
+    return dumps(auth_logout(data['token']))
+
+@APP.route('/auth/register', methods=['POST'])
+def server_register():
+    data = request.get_json()
+    return dumps(auth_register(data['email'], data['password'], data['name_first'], \
+        data['name_last']))
+
 if __name__ == "__main__":
     APP.run(port=0) # Do not edit this port
