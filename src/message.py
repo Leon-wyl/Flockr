a0abaffc5a43a457fa8366a65ac300021ac26065
Yilang Wu <z5314144@ad.unsw.edu.au>
2020-11-08 01:06:34 +1100

    modify message_pin and unpin in database.py message.py utility.py

--- src/message.py
+++ src/message.py
@@ -3,7 +3,7 @@ from error import AccessError
 from database import *
 from utility import *
 from auth import *
-from database import data
+from database import data 
 
 
 def message_send(token, channel_id, message):
@@ -30,19 +30,18 @@ def message_edit(token, message_id, message):
     data_message_edit(channel_id, message_id, message)
     
 def message_pin(token, message_id):
-    check_valid_token(token)
-    u_id = auth_u_id_from_token(token)
-    channel_id = data_get_channel_id(message_id)
-    check_authorised_member_channel(channel_id, u_id)
-    if not is_owner_exist(u_id, channel_id):
-        check_global_owner(u_id)
+    channel_id = prior_check_pin_unpin(token, message_id)
     check_message_pinned(message_id, channel_id)
 
 def message_unpin(token, message_id):
+    channel_id = prior_check_pin_unpin(token, message_id)
+    check_message_unpinned(message_id, channel_id)
+
+def prior_check_pin_unpin(token, message_id):
     check_valid_token(token)
     u_id = auth_u_id_from_token(token)
     channel_id = data_get_channel_id(message_id)
     check_authorised_member_channel(channel_id, u_id)
     if not is_owner_exist(u_id, channel_id):
         check_global_owner(u_id)
-    check_message_unpinned(message_id, channel_id)
+    return channel_id
