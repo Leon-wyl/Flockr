5148c7886029e72e63f7b63cb9e9d4e220672b00
Alex Duan <haoheng.duan@student.unsw.edu.au>
2020-11-08 20:15:26 +1100

    add tests to standup functions in other.py

--- src/other_test.py
+++ src/other_test.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from other import *
 from auth import *
 from channels import *
@@ -28,23 +29,23 @@ def test_users_all():
             'name_last': "Johnson",
             'handle_str': 'mjohnson',
          }
-        
+
     ]}
-    
-# Test invalid token    
+
+# Test invalid token
 def test_users_all_except():
     clear()
     auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
     info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
     with pytest.raises(AccessError):
         users_all(info2['token'] + 'a')
-    
+
 # test the number of channels and users in database
 def test_clear():
     clear()
     assert channel_numbers() == 0
-    assert data_u_id() == 0       
- 
+    assert data_u_id() == 0
+
 # test if the admin_userpermission_change correctly
 def test_admin_userpermission_change():
     clear()
@@ -53,36 +54,36 @@ def test_admin_userpermission_change():
     assert data_permission(1) == 2
     admin_userpermission_change(info1['token'], 1, 1)
     assert data_permission(1) == 1
- 
+
 def test_admin_userpermission_change_invalid_token():
     clear()
     info1 = auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
     auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
     with pytest.raises(AccessError):
         admin_userpermission_change(info1['token'] + 'a', 1, 1)
- 
+
 def test_admin_userpermission_change_invalid_user():
     clear()
     info1 = auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
     auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
     with pytest.raises(InputError):
-        admin_userpermission_change(info1['token'], 3, 1) 
- 
+        admin_userpermission_change(info1['token'], 3, 1)
+
 def test_admin_userpermission_change_invalid_permission():
     clear()
     info1 = auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
     auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
     with pytest.raises(InputError):
-        admin_userpermission_change(info1['token'], 1, 3) 
- 
+        admin_userpermission_change(info1['token'], 1, 3)
+
 def test_admin_userpermission_change_not_owner():
     clear()
     auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
     info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
     with pytest.raises(AccessError):
-        admin_userpermission_change(info2['token'], 0, 2)    
-    
-# test if the function return correct messages   
+        admin_userpermission_change(info2['token'], 0, 2)
+
+# test if the function return correct messages
 def test_search():
     clear()
     info1 = auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
@@ -121,7 +122,7 @@ def test_search():
             'u_id': 1,
             'message': "ok in abcdefg",
             'time_created': 0,
-        },   
+        },
     ]}
 
 # test if function raises Exception if the token is invalid
@@ -140,3 +141,124 @@ def test_search_except():
     with pytest.raises(AccessError):
         search(info2['token'] + 'a', 'ok')
 
+def test_standup_start():
+    clear()
+    info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    channels_create(info2['token'], 'first', True)
+    time = datetime.datetime.now() + datetime.timedelta(seconds=6)
+    assert standup_start(info2['token'], 0, 6) == {
+        time_finish: time.replace(tzinfo=timezone.utc).timestamp(),
+    }
+
+def test_standup_start_invalid_channel():
+    clear()
+    info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    channels_create(info2['token'], 'first', True)
+    with pytest.raises(InputError):
+        standup_start(info2['token'], 2, 6)
+        standup
+def test_standup_start_already_active():
+    clear()
+    info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    channels_create(info2['token'], 'first', True)
+    standup_start(info2['token'], 0, 100)
+    with pytest.raises(InputError):
+        standup_start(info2['token'], 0, 6)
+
+def test_standup_start_invalid_token():
+    clear()
+    info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    channels_create(info2['token'], 'first', True)
+    with pytest.raises(AccessError):
+        standup_start(info2['token'] + 'a', 0, 6)
+
+
+def test_standup_active():
+    clear()
+    info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    channels_create(info2['token'], 'first', True)
+    time = datetime.datetime.now() + datetime.timedelta(seconds=20)
+    standup_start(info2['token'], 0, 20)
+    assert standup_active(info2['token'], 0) == {
+        is_active: True,
+        time_finish = time.replace(tzinfo=timezone.utc).timestamp(),
+    }
+
+
+
+def test_standup_active_invalid_channel():
+    clear()
+    info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    channels_create(info2['token'], 'first', True)
+    time = datetime.datetime.now() + datetime.timedelta(seconds=20)
+    standup_start(info2['token'], 0, 20)
+    with pytest.raises(InputError):
+        standup_active(info2['token'], 2)
+
+
+
+def test_standup_active_invalid_token():
+    clear()
+    info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    channels_create(info2['token'], 'first', True)
+    time = datetime.datetime.now() + datetime.timedelta(seconds=20)
+    standup_start(info2['token'], 0, 20)
+    with pytest.raises(AccessError):
+        standup_active(info2['token'] + 'a', 0)
+
+
+def test_standup_send():
+    clear()
+    info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    channels_create(info2['token'], 'first', True)
+    standup_start(info2['token'], 0, 20)
+    standup_send(info2['token'], 0, 'hello')
+    standup_send(info2['token'], 0, 'asd')
+    standup_send(info2['token'], 0, 'dfg')
+    standup_send(info2['token'], 0, 'abc')
+    assert
+
+def test_standup_send_invalid_channel():
+    clear()
+    info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    channels_create(info2['token'], 'first', True)
+    standup_start(info2['token'], 0, 20)
+    with pytest.raises(InputError):
+        standup_send(info2['token'], 2, 'abc')
+
+
+def test_standup_send_invalid_message():
+    clear()
+    info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    channels_create(info2['token'], 'first', True)
+    standup_start(info2['token'], 0, 20)
+    with pytest.raises(InputError):
+        standup_send(info2['token'], 0, 'abc'*1000)
+
+
+def test_standup_send_not_active():
+    clear()
+    info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    channels_create(info2['token'], 'first', True)
+    with pytest.raises(InputError):
+        standup_send(info2['token'], 0, 'abc')
+
+def test_standup_send_not_member():
+    clear()
+    info1 = auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
+    info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    channels_create(info2['token'], 'first', True)
+    standup_start(info2['token'], 0, 20)
+    with pytest.raises(AccessError):
+        standup_send(info1['token'], 0, 'abc')
+
+
+
+
+def test_standup_send_invalid_token():
+    clear()
+    info2 = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    channels_create(info2['token'], 'first', True)
+    standup_start(info2['token'], 0, 20)
+    with pytest.raises(AccessError):
+        standup_send(info2['token'] + 'a', 0, 'abc')
