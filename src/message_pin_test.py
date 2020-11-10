f9a7cd96fa3fcc15fa37c11b039866d386a083f1
Yilang Wu <z5314144@ad.unsw.edu.au>
2020-11-10 23:43:11 +1100

    tests for sendlater implemented in message_pin_test.py

--- src/message_pin_test.py
+++ src/message_pin_test.py
@@ -1,10 +1,12 @@
 from auth import auth_register
 from channels import channels_create
 from channel import channel_join, channel_messages
-from message import message_send, message_pin, message_unpin
+from message import message_send, message_pin, message_unpin, message_sendlater
 from other import clear
 from error import InputError, AccessError
+import threading
 import pytest
+import datetime
 
 def test_message_pin_valid0():
     '''Owner of the channel pin the message sent by a member'''
@@ -415,3 +417,205 @@ def test_message_unpin_invalid3():
     with pytest.raises(AccessError):
         # User 2 pin the message sent by himself
         message_unpin(user2_info['token'], message0_info['message_id'])
+
+def test_message_sendlater_valid0():
+    "User 1 sent a message 0 second later"
+    clear()
+
+    # Register user 0
+    user0_info = auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
+    # Register user 1
+    user1_info = auth_register("billgates@outlook.com", "VukkFs", "Bill", "Gates")
+    # Register user 2
+    user2_info = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    # User 0 create a channel
+    channel0_info = channels_create(user0_info['token'], "channel0", True)
+    # User 1 join the channel
+    channel_join(user1_info['token'], channel0_info['channel_id'])
+    # User 1 create another channel
+    channel1_info = channels_create(user1_info['token'], "channel1", True)
+    # User 2 join channel1
+    channel_join(user2_info['token'], channel1_info['channel_id'])
+    # User 0 join channel1
+    channel_join(user0_info['token'], channel1_info['channel_id'])
+    # User 2 send a message
+    message_send(user2_info['token'], channel1_info['channel_id'], "Hello")
+    # USer 1 send a message 0 second later
+    zero_sec_later = datetime.datetime.now()
+    timestamp = int(datetime.datetime.timestamp(zero_sec_later))
+    message_sendlater(user1_info['token'], channel1_info['channel_id'], "Hi", timestamp)
+    # User 0 get the info of messages
+    message_info = channel_messages(user0_info['token'], channel1_info['channel_id'], 0)
+    assert message_info['messages'][1]['message'] == "Hi"
+
+def test_message_sendlater_valid1():
+    "User 1 sent a message 1 second later"
+    clear()
+
+    # Register user 0
+    user0_info = auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
+    # Register user 1
+    user1_info = auth_register("billgates@outlook.com", "VukkFs", "Bill", "Gates")
+    # Register user 2
+    user2_info = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    # User 0 create a channel
+    channel0_info = channels_create(user0_info['token'], "channel0", True)
+    # User 1 join the channel
+    channel_join(user1_info['token'], channel0_info['channel_id'])
+    # User 1 create another channel
+    channel1_info = channels_create(user1_info['token'], "channel1", True)
+    # User 2 join channel1
+    channel_join(user2_info['token'], channel1_info['channel_id'])
+    # User 0 join channel1
+    channel_join(user0_info['token'], channel1_info['channel_id'])
+    # User 2 send a message
+    message_send(user2_info['token'], channel1_info['channel_id'], "Hello")
+    # USer 1 send a message 1 second later
+    one_sec_later = datetime.datetime.now() + datetime.timedelta(seconds=1)
+    timestamp = int(datetime.datetime.timestamp(one_sec_later))
+    message_sendlater(user1_info['token'], channel1_info['channel_id'], "Hi", timestamp)
+    # User 0 get the info of messages 1 sec later
+    t = threading.Timer(1, one_sec_later_function(user0_info, channel1_info))
+    t.start()
+
+def one_sec_later_function(user0_info, channel1_info):
+    message_info = channel_messages(user0_info['token'], channel1_info['channel_id'], 0)
+    assert message_info['messages'][1]['message'] == "Hi"
+
+def test_message_sendlater_valid2():
+    '''User 1 sent a message "'H' * 1000" 0 second later'''
+    clear()
+
+    # Register user 0
+    user0_info = auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
+    # Register user 1
+    user1_info = auth_register("billgates@outlook.com", "VukkFs", "Bill", "Gates")
+    # Register user 2
+    user2_info = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    # User 0 create a channel
+    channel0_info = channels_create(user0_info['token'], "channel0", True)
+    # User 1 join the channel
+    channel_join(user1_info['token'], channel0_info['channel_id'])
+    # User 1 create another channel
+    channel1_info = channels_create(user1_info['token'], "channel1", True)
+    # User 2 join channel1
+    channel_join(user2_info['token'], channel1_info['channel_id'])
+    # User 0 join channel1
+    channel_join(user0_info['token'], channel1_info['channel_id'])
+    # User 2 send a message
+    message_send(user2_info['token'], channel1_info['channel_id'], "Hello")
+    # USer 1 send a message 0 second later with invalid channel_id
+    zero_sec_later = datetime.datetime.now()
+    timestamp = int(datetime.datetime.timestamp(zero_sec_later))
+    message_sendlater(user1_info['token'], channel1_info['channel_id'], "H" * 1000, timestamp)
+    # User 1 get the message info
+    message_info = channel_messages(user1_info['token'], channel1_info['channel_id'], 0)
+    assert message_info['messages'][1]['message'] == "H" * 1000
+
+def test_message_sendlater_invalid0():
+    "User 1 sent a message 0 second later in a invalid channel"
+    clear()
+
+    # Register user 0
+    user0_info = auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
+    # Register user 1
+    user1_info = auth_register("billgates@outlook.com", "VukkFs", "Bill", "Gates")
+    # Register user 2
+    user2_info = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    # User 0 create a channel
+    channel0_info = channels_create(user0_info['token'], "channel0", True)
+    # User 1 join the channel
+    channel_join(user1_info['token'], channel0_info['channel_id'])
+    # User 1 create another channel
+    channel1_info = channels_create(user1_info['token'], "channel1", True)
+    # User 2 join channel1
+    channel_join(user2_info['token'], channel1_info['channel_id'])
+    # User 0 join channel1
+    channel_join(user0_info['token'], channel1_info['channel_id'])
+    # User 2 send a message
+    message_send(user2_info['token'], channel1_info['channel_id'], "Hello")
+    # USer 1 send a message 0 second later with invalid channel_id
+    zero_sec_later = datetime.datetime.now()
+    timestamp = int(datetime.datetime.timestamp(zero_sec_later))
+    with pytest.raises(InputError):
+        message_sendlater(user1_info['token'], 2, "Hi", timestamp)
+
+def test_message_sendlater_invalid1():
+    "User 1 sent a message 0 second later in a invalid channel"
+    clear()
+
+    # Register user 0
+    user0_info = auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
+    # Register user 1
+    user1_info = auth_register("billgates@outlook.com", "VukkFs", "Bill", "Gates")
+    # Register user 2
+    user2_info = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    # User 0 create a channel
+    channel0_info = channels_create(user0_info['token'], "channel0", True)
+    # User 1 join the channel
+    channel_join(user1_info['token'], channel0_info['channel_id'])
+    # User 1 create another channel
+    channel1_info = channels_create(user1_info['token'], "channel1", True)
+    # User 2 join channel1
+    channel_join(user2_info['token'], channel1_info['channel_id'])
+    # User 0 join channel1
+    channel_join(user0_info['token'], channel1_info['channel_id'])
+    # User 2 send a message
+    message_send(user2_info['token'], channel1_info['channel_id'], "Hello")
+    # USer 1 send a message 0 second later with invalid channel_id
+    zero_sec_later = datetime.datetime.now()
+    timestamp = int(datetime.datetime.timestamp(zero_sec_later))
+    with pytest.raises(InputError):
+        message_sendlater(user1_info['token'], channel1_info, "H" * 1001, timestamp)
+
+def test_message_sendlater_invalid2():
+    "User 1 sent a message 0 second later"
+    clear()
+
+    # Register user 0
+    user0_info = auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
+    # Register user 1
+    user1_info = auth_register("billgates@outlook.com", "VukkFs", "Bill", "Gates")
+    # Register user 2
+    user2_info = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    # User 0 create a channel
+    channel0_info = channels_create(user0_info['token'], "channel0", True)
+    # User 1 join the channel
+    channel_join(user1_info['token'], channel0_info['channel_id'])
+    # User 1 create another channel
+    channel1_info = channels_create(user1_info['token'], "channel1", True)
+    # User 2 join channel1
+    channel_join(user2_info['token'], channel1_info['channel_id'])
+    # User 0 join channel1
+    channel_join(user0_info['token'], channel1_info['channel_id'])
+    # User 2 send a message
+    message_send(user2_info['token'], channel1_info['channel_id'], "Hello")
+    # USer 1 send a message 1 second before
+    one_sec_before = datetime.datetime.now() - datetime.timedelta(seconds=1)
+    timestamp = int(datetime.datetime.timestamp(one_sec_before))
+    with pytest.raises(InputError):
+        message_sendlater(user1_info['token'], channel1_info['channel_id'], "Hi", timestamp)
+
+def test_message_pin_invalid3():
+    '''the authorised user has not joined the channel they are trying to post to'''
+    clear()
+    user0_info = auth_register("leonwu@gmail.com", "ihfeh3hgi00d", "Yilang", "W")
+    # Register user 1
+    user1_info = auth_register("billgates@outlook.com", "VukkFs", "Bill", "Gates")
+    # Register user 2
+    user2_info = auth_register("johnson@icloud.com", "RFVtgb45678", "M", "Johnson")
+    # User 0 create a channel
+    channel0_info = channels_create(user0_info['token'], "channel0", True)
+    # User 1 join the channel
+    channel_join(user1_info['token'], channel0_info['channel_id'])
+    # User 1 create another channel
+    channel1_info = channels_create(user1_info['token'], "channel1", True)
+    # User 2 join channel1
+    channel_join(user2_info['token'], channel1_info['channel_id'])
+    # User 2 send a message
+    message_send(user2_info['token'], channel1_info['channel_id'], "Hello")
+    # USer 1 send a message 0 second later
+    zero_sec_later = datetime.datetime.now()
+    timestamp = int(datetime.datetime.timestamp(zero_sec_later))
+    with pytest.raises(AccessError):
+        message_sendlater(user0_info['token'], channel1_info['channel_id'], "Hi", timestamp)
